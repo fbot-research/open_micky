daef7fba6d65a3b6e851629b075b38d7be3c2019
Gabriel Torres <gabrieltlt721@gmail.com>
2026-03-23 17:40:59 -0300

    :memo: updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Open_MICKY'
 copyright = '2026, FBOT@Research'
 author = 'FBOT@Research'
 release = '0.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["myst_parser"]
 
@@ -26,11 +15,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 myst_enable_extensions = [
     "colon_fence",
     "deflist",
-    "html_admonition",
+    "table",
 ]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+myst_heading_anchors = 3
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+
+# opcional (remova se não criar a pasta)
 html_static_path = ['_static']
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#2B5D9F",
+        "color-brand-content": "#2B5D9F",
+    },
+}
